dcbd2c53006dcefdaba1490d7f33e6259a6d4b1d
sbx_user1051 <sbx_user1051@169.254.31.73>
2026-01-28 21:54:16 +0000

    Wed Jan 28 21:54:16 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,22 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
+
+    public Map<String, Integer> countSkuLetters(String skus) {
+        Map<String, Integer> skuCount = new HashMap<>();
+
+        for (char c : skus.toCharArray()){
+            if (Character.isLetter(c)) {
+                String letter = String.valueOf(C)
+            }
+        }
+    }
 }
+
