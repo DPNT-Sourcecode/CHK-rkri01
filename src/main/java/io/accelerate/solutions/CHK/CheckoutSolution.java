c16d251077d693dae52ee8bcd7d82dcf4aec7019
sbx_user1051 <sbx_user1051@169.254.31.73>
2026-01-28 22:03:16 +0000

    Wed Jan 28 22:03:16 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -6,21 +6,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
+
+    private HashMap<String, Integer> prices ;
+
     public Integer checkout(String skus) {
         Map<String, Integer> skuCount = new HashMap<>();
         for (char c : skus.toCharArray()) {
-            if (Character.isLetter(c)) {
-                skuCount.put(String.valueOf(c), )
+            if (!Character.isLetter(c)) {
+                return -1;
+            } else {
+                String letter = String.valueOf(c);
+                skuCount.put(letter, skuCount.getOrDefault(letter, 0) + 1);
             }
-        } return -1;
+        }
     }
 
-    public Map<String, Integer> countSkuLetters(String skus) {
+    private HashMap<String, Integer> populatePrices(HashMap<String, Integer> prices) {
+        HashMap<String, Integer> pricesPopulated = new HashMap<>();
+        pricesPopulated.put("A", 50);
+        pricesPopulated.put("B", 50);
+        pricesPopulated.put("C", 50);
+        pricesPopulated.put("D", 15);
 
-
-
-        }
-
+    }
 }
+
 
 
