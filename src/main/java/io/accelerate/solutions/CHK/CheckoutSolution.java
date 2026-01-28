8e8c81badcbadb31584412b9360a74945da02e8e
sbx_user1051 <sbx_user1051@169.254.31.73>
2026-01-28 21:57:16 +0000

    Wed Jan 28 21:57:16 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,17 +7,20 @@ import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        Map<String, Integer> skuCount = new HashMap<>();
+        for (char c : skus.toCharArray()) {
+            if (Character.isLetter(c)) {
+                skuCount.put(String.valueOf(c), )
+            }
+        } return -1;
     }
 
     public Map<String, Integer> countSkuLetters(String skus) {
-        Map<String, Integer> skuCount = new HashMap<>();
 
-        for (char c : skus.toCharArray()){
-            if (Character.isLetter(c)) {
-                String letter = String.valueOf(C)
-            }
+
+
         }
-    }
+
 }
+
 
