2f22b23a575e6e7f858959aa1eb238fd9b0ea603
sbx_user1051 <sbx_user1051@169.254.32.165>
2026-01-29 14:46:02 +0000

    Thu Jan 29 14:46:02 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckSku.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckSku.java
@@ -1,4 +1,15 @@
 package io.accelerate.solutions.CHK;
 
 public class CheckSku {
+
+    public static boolean isValid (String letter, boolean isValid) {
+        if  (letter.equals("A") || letter.equals("B") || letter.equals("C") || letter.equals("D")
+                || letter.equals("E") || letter.equals("F")) {
+            isValid = true;
+        } else {
+            isValid = false;
+        }
+        return isValid;
+    }
 }
+
