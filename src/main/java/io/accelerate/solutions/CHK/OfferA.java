8805f85ec83ebd8fcfc9b67200db78ff8ada43be
sbx_user1051 <sbx_user1051@169.254.32.165>
2026-01-29 14:34:02 +0000

    Thu Jan 29 14:34:02 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/OfferA.java
+++ src/main/java/io/accelerate/solutions/CHK/OfferA.java
@@ -1,5 +1,6 @@
 package io.accelerate.solutions.CHK;
 
-public record OfferA(int offerA, int fullPricedA) {
+public record OfferA(int offerA, int fullPricedA, int offerA2, int fullPriceA2) {
 }
+
 
---
+++ src/main/java/io/accelerate/solutions/CHK/OfferB.java
@@ -0,0 +1,4 @@
+package io.accelerate.solutions.CHK;
+
+public record OfferB(int offerB, int fullPricedB) {
+}
